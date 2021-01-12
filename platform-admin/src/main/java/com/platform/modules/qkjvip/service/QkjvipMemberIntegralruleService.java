aef8d8e8e281818b535ba59c7cf77e972ae9bf08
liuqianru <939350807@qq.com>
2021-01-12 15:05:00 +0800

    qianru
    积分规则追加

---
+++ platform-admin/src/main/java/com/platform/modules/qkjvip/service/QkjvipMemberIntegralruleService.java
@@ -0,0 +1,76 @@
+/*
+ * 项目名称:platform-plus
+ * 类名称:QkjvipMemberIntegralruleService.java
+ * 包名称:com.platform.modules.qkjvip.service
+ *
+ * 修改履历:
+ *     日期                       修正者        主要内容
+ *     2021-01-12 10:05:39        liuqianru     初版做成
+ *
+ * Copyright (c) 2019-2019 微同软件
+ */
+package com.platform.modules.qkjvip.service;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.platform.modules.qkjvip.entity.QkjvipMemberIntegralruleEntity;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Service接口
+ *
+ * @author liuqianru
+ * @date 2021-01-12 10:05:39
+ */
+public interface QkjvipMemberIntegralruleService extends IService<QkjvipMemberIntegralruleEntity> {
+
+    /**
+     * 查询所有列表
+     *
+     * @param params 查询参数
+     * @return List
+     */
+    List<QkjvipMemberIntegralruleEntity> queryAll(Map<String, Object> params);
+
+    /**
+     * 分页查询
+     *
+     * @param params 查询参数
+     * @return Page
+     */
+    Page queryPage(Map<String, Object> params);
+
+    /**
+     * 新增
+     *
+     * @param qkjvipMemberIntegralrule 
+     * @return 新增结果
+     */
+    boolean add(QkjvipMemberIntegralruleEntity qkjvipMemberIntegralrule);
+
+    /**
+     * 根据主键更新
+     *
+     * @param qkjvipMemberIntegralrule 
+     * @return 更新结果
+     */
+    boolean update(QkjvipMemberIntegralruleEntity qkjvipMemberIntegralrule);
+
+    /**
+     * 根据主键删除
+     *
+     * @param id id
+     * @return 删除结果
+     */
+    boolean delete(String id);
+
+    /**
+     * 根据主键批量删除
+     *
+     * @param ids ids
+     * @return 删除结果
+     */
+    boolean deleteBatch(String[] ids);
+}
---
+++ platform-admin/src/main/java/com/platform/modules/qkjvip/service/impl/QkjvipMemberIntegralruleServiceImpl.java
@@ -0,0 +1,70 @@
+/*
+ * 项目名称:platform-plus
+ * 类名称:QkjvipMemberIntegralruleServiceImpl.java
+ * 包名称:com.platform.modules.qkjvip.service.impl
+ *
+ * 修改履历:
+ *     日期                       修正者        主要内容
+ *     2021-01-12 10:05:39        liuqianru     初版做成
+ *
+ * Copyright (c) 2019-2019 微同软件
+ */
+package com.platform.modules.qkjvip.service.impl;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.platform.common.utils.Query;
+import com.platform.modules.qkjvip.dao.QkjvipMemberIntegralruleDao;
+import com.platform.modules.qkjvip.entity.QkjvipMemberIntegralruleEntity;
+import com.platform.modules.qkjvip.service.QkjvipMemberIntegralruleService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Service实现类
+ *
+ * @author liuqianru
+ * @date 2021-01-12 10:05:39
+ */
+@Service("qkjvipMemberIntegralruleService")
+public class QkjvipMemberIntegralruleServiceImpl extends ServiceImpl<QkjvipMemberIntegralruleDao, QkjvipMemberIntegralruleEntity> implements QkjvipMemberIntegralruleService {
+
+    @Override
+    public List<QkjvipMemberIntegralruleEntity> queryAll(Map<String, Object> params) {
+        return baseMapper.queryAll(params);
+    }
+
+    @Override
+    public Page queryPage(Map<String, Object> params) {
+        //排序
+        params.put("sidx", "T.id");
+        params.put("asc", false);
+        Page<QkjvipMemberIntegralruleEntity> page = new Query<QkjvipMemberIntegralruleEntity>(params).getPage();
+        return page.setRecords(baseMapper.selectQkjvipMemberIntegralrulePage(page, params));
+    }
+
+    @Override
+    public boolean add(QkjvipMemberIntegralruleEntity qkjvipMemberIntegralrule) {
+        return this.save(qkjvipMemberIntegralrule);
+    }
+
+    @Override
+    public boolean update(QkjvipMemberIntegralruleEntity qkjvipMemberIntegralrule) {
+        return this.updateById(qkjvipMemberIntegralrule);
+    }
+
+    @Override
+    public boolean delete(String id) {
+        return this.removeById(id);
+    }
+
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public boolean deleteBatch(String[] ids) {
+        return this.removeByIds(Arrays.asList(ids));
+    }
+}
