5b60fe615d85085cd38d28d2362211ad5eb29266
liuqianru <939350807@qq.com>
2021-05-08 17:28:11 +0800

    qianru

--- platform-admin/src/main/java/com/platform/modules/qkjvip/controller/MemberController.java
+++ platform-admin/src/main/java/com/platform/modules/qkjvip/controller/MemberController.java
@@ -382,7 +382,6 @@ public class MemberController extends AbstractController {
     @SysLog("导入会员")
     @RequestMapping("/import")
     @RequiresPermissions("qkjvip:member:import")
-    @Transactional(rollbackFor = Exception.class)
     public RestResponse importExcel(MultipartFile file) {
         String fileName = file.getOriginalFilename();
         if (StringUtils.isBlank(fileName)) {
@@ -408,7 +407,6 @@ public class MemberController extends AbstractController {
                     list.get(i).setOfflineflag(1);
                 }
                 if (list.size() > 0) {
-                    Object savePoint = TransactionAspectSupport.currentTransactionStatus().createSavepoint();
                     qkjvipMemberImportService.addBatch(list); //批量导入临时表
 
                     //调用数据清洗接口
@@ -418,7 +416,6 @@ public class MemberController extends AbstractController {
 
                     JSONObject resultObject = JSON.parseObject(resultPost);
                     if (!"200".equals(resultObject.get("resultcode").toString())) {  //清洗失败
-                        TransactionAspectSupport.currentTransactionStatus().rollbackToSavepoint(savePoint);
                         return RestResponse.error(resultObject.get("descr").toString());
                     }
                 }
